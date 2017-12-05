7cbf08a10df6d5c7bd984795d21a1ed7dd74ac62
Hiroki Teranishi <teranishihiroki@gmail.com>
2017-12-05 19:17:00 +0900

    Implement Parameter.add_parameter.

--- src/optimizer.rs
+++ src/optimizer.rs
@@ -1,5 +1,15 @@
 extern crate primitiv_sys as _primitiv;
 
+use Parameter;
 use Wrap;
 
-pub trait Optimizer: Wrap<_primitiv::primitiv_Optimizer> {}
+pub trait Optimizer: Wrap<_primitiv::primitiv_Optimizer> {
+    fn add_parameter(&mut self, param: &mut Parameter) {
+        unsafe {
+            _primitiv::primitiv_Optimizer_add_parameter(
+                self.as_inner_mut_ptr(),
+                param.as_inner_mut_ptr(),
+            );
+        }
+    }
+}
