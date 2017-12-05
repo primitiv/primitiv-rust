b5b52d9baf2b8387f620bae453d8a2aae781ae09
Hiroki Teranishi <teranishihiroki@gmail.com>
2017-12-05 19:20:29 +0900

    Reimplement SGD::new().

--- src/optimizers/optimizer_impl.rs
+++ src/optimizers/optimizer_impl.rs
@@ -1,7 +1,7 @@
 extern crate primitiv_sys as _primitiv;
 
+use Optimizer;
 use Wrap;
-use optimizer;
 
 #[derive(Debug)]
 pub struct SGD {
@@ -11,4 +11,14 @@ pub struct SGD {
 impl_wrap!(SGD, primitiv_Optimizer);
 impl_drop!(SGD, primitiv_SGD_delete);
 
-impl optimizer::Optimizer for SGD {}
+impl Optimizer for SGD {}
+
+impl SGD {
+    pub fn new(eta: f32) -> Self {
+        unsafe {
+            let inner = _primitiv::primitiv_SGD_new_with_eta(eta);
+            assert!(!inner.is_null());
+            SGD { inner: inner }
+        }
+    }
+}
