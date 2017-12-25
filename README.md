96dde6c478d8fb64ffc945d41c9c7d8a957b2603
Hiroki Teranishi <teranishihiroki@gmail.com>
2017-12-25 16:56:37 +0900

    Add installation explanation.

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 Rust frontend of primitiv
 =================================
 
+Prerequisites
+-------------
+
+* Rust (1.22 or later)
+* Clang (3.9 or later)
+* (optional) CUDA (7.5 or later)
+
+Install
+---------------
+
+```
+mkdir work
+cd work
+
+# build primitiv core with C API
+git clone https://github.com/primitiv/primitiv/
+cd primitiv
+git checkout dc022e3fd4c343f7b46f2d04698f940211bca773
+mkdir build
+cd build
+cmake .. -DPRIMITIV_BUILD_C_API=ON [-DPRIMITIV_USE_CUDA=ON]
+make [-j <threads>]
+[make install]
+
+# build primitiv-rust
+cd ../../
+git clone https://github.com/primitiv/primitiv-rust/
+[PRIMITIV_INCLUDE_DIR=/path/to/work/primitiv PRIMITIV_LIBRARY_DIR=/path/to/work/primitiv/build/primitiv] cargo build [--features cuda]
+
+# try xor example
+[PRIMITIV_INCLUDE_DIR=/path/to/work/primitiv PRIMITIV_LIBRARY_DIR=/path/to/work/primitiv/build/primitiv] cargo build --example xor
+[LD_LIBRARY_PATH=/path/to/work/primitiv/build/primitiv] cargo run --example xor
+```
+
--- primitiv-sys/build.rs
+++ primitiv-sys/build.rs
@@ -16,21 +16,19 @@ fn main() {
 }
 
 fn build() -> Result<(), Box<Error>> {
-    let lib_dir = env::var("PRIMITIV_C_LIBRARY_DIR").unwrap_or("/usr/local/lib".to_string());
-    let include_dir =
-        env::var("PRIMITIV_C_INCLUDE_DIR").unwrap_or("/usr/local/include".to_string());
+    let lib_dir = env::var("PRIMITIV_LIBRARY_DIR").unwrap_or("/usr/local/lib".to_string());
+    let include_dir = env::var("PRIMITIV_INCLUDE_DIR").unwrap_or("/usr/local/include".to_string());
     println!("cargo:rustc-link-lib=dylib=primitiv_c");
     println!("cargo:rustc-link-search={}", lib_dir);
 
     let mut builder = bindgen::Builder::default()
         .clang_arg(format!("-I{}", include_dir))
-        .header(format!("{}/primitiv_c/api.h", include_dir))
+        .header(format!("{}/primitiv/c/api.h", include_dir))
         .rustfmt_bindings(false)
         .generate_comments(false);
 
     if cfg!(feature = "cuda") {
-        builder = builder
-            .header(format!("{}/primitiv_c/api_cuda.h", include_dir));
+        builder = builder.header(format!("{}/primitiv/c/api_cuda.h", include_dir));
     }
 
     builder
