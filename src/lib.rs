d0509662b0a613726b7c60b7554e84ef958680cf
Hiroki Teranishi <teranishihiroki@gmail.com>
2018-02-05 06:43:39 +0900

    Fix lib.rs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,129 +1,35 @@
 extern crate primitiv_sys;
 extern crate libc;
 
-use std::fmt::{Debug, Display, Error, Formatter};
-
-pub trait Wrap<T>: Drop {
-    fn from_raw(ptr: *mut T, owned: bool) -> Self;
-    fn as_ptr(&self) -> *const T;
-    fn as_mut_ptr(&mut self) -> *mut T;
-    fn is_owned(&self) -> bool;
-}
-
-macro_rules! impl_wrap {
-    ($name:ident, $type:ident) => {
-        impl Wrap<_primitiv::$type> for $name {
-            #[inline(always)]
-            fn from_raw(ptr: *mut _primitiv::$type, owned: bool) -> Self {
-                $name { inner: ptr, owned: owned }
-            }
-
-            #[inline(always)]
-            fn as_ptr(&self) -> *const _primitiv::$type {
-                self.inner
-            }
-
-            #[inline(always)]
-            fn as_mut_ptr(&mut self) -> *mut _primitiv::$type {
-                self.inner
-            }
-
-            #[inline(always)]
-            fn is_owned(&self) -> bool {
-                self.owned
-            }
-        }
-    }
-}
-
-macro_rules! impl_wrap_owned {
-    ($name:ident, $type:ident) => {
-        impl Wrap<_primitiv::$type> for $name {
-            #[inline(always)]
-            fn from_raw(ptr: *mut _primitiv::$type, owned: bool) -> Self {
-                $name { inner: ptr }
-            }
-
-            #[inline(always)]
-            fn as_ptr(&self) -> *const _primitiv::$type {
-                self.inner
-            }
-
-            #[inline(always)]
-            fn as_mut_ptr(&mut self) -> *mut _primitiv::$type {
-                self.inner
-            }
-
-            #[inline(always)]
-            fn is_owned(&self) -> bool {
-                true
-            }
-        }
-    }
-}
-
-/*
-macro_rules! impl_new {
-    ($name:ident, $call:ident) => {
-        impl $name {
-            pub fn new() -> Self {
-                // unsafe {
-                //     let inner = _primitiv::$call();
-                //     assert!(!inner.is_null());
-                //     $name { inner: inner, owned: true }
-                // }
-                unsafe {
-                    let mut inner:  = ptr::null_mut();
-                    check_api_status!(_primitiv::$call(&mut inner));
-                    assert!(!inner.is_null());
-                    $name { inner: inner, owned: true }
-                }
-            }
-        }
-    }
-}
-*/
-
-macro_rules! impl_drop {
-    ($name:ident, $call:ident) => {
-        impl Drop for $name {
-            fn drop(&mut self) {
-                if self.is_owned() {
-                    unsafe {
-                        _primitiv::$call(self.inner);
-                    }
-                }
-            }
-        }
-    }
-}
 #[macro_use]
 mod status;
-pub use status::*;
+pub(crate) use status::*;
 
 #[macro_use]
-pub mod device;
-// pub use device::*;
+mod util;
+pub use util::*;
+#[macro_use]
+mod device;
+pub use device::Device;
 mod graph;
-pub use graph::*;
+pub use graph::{Graph, Node};
 #[macro_use]
 mod initializer;
-pub use initializer::*;
-// mod parameter;
-// pub use parameter::*;
+pub use initializer::Initializer;
+mod model;
+pub use model::Model;
+mod parameter;
+pub use parameter::Parameter;
 mod shape;
-pub use shape::*;
-// #[macro_use(check_api_status)]
-// mod status;
-// pub use status::*;
+pub use shape::Shape;
 mod tensor;
-pub use tensor::*;
-// mod optimizer;
-// pub use optimizer::*;
-// // pub mod functions;
-// // pub use functions::node_funcs as node_functions;
-// // pub use functions::tensor_funcs as tensor_functions;
-//
+pub use tensor::Tensor;
+#[macro_use]
+mod optimizer;
+pub use optimizer::Optimizer;
+pub mod functions;
+pub use functions::node_funcs as node_functions;
+pub use functions::tensor_funcs as tensor_functions;
 pub mod devices;
 pub mod initializers;
-// // pub mod optimizers;
+pub mod optimizers;
