f9b5c147a933f8d1d0b38b8e4ecce1ffebb893e4
Hiroki Teranishi <teranishihiroki@gmail.com>
2018-02-05 06:39:09 +0900

    Implement Status.

--- src/status.rs
+++ src/status.rs
@@ -1,40 +1,18 @@
 extern crate backtrace;
-
 use primitiv_sys as _primitiv;
 use libc::c_uint;
-use self::backtrace::{Backtrace, BacktraceFrame, BacktraceSymbol};
-use std::error::Error;
-use std::ffi::CStr;
+use self::backtrace::Backtrace;
 use std::ffi::CString;
-use std::ffi::NulError;
+use std::fmt;
 use std::fmt::Debug;
 use std::fmt::Display;
 use std::fmt::Formatter;
-use std::fmt;
 use std::mem;
-use std::os::raw::c_char;
 use std::ptr;
 use std::result;
-use std::str::Utf8Error;
-use Wrap;
-
-fn prev_symbol(level: u32) -> Option<BacktraceSymbol> {
-    let (trace, curr_file, curr_line) = (Backtrace::new(), file!(), line!());
-    let frames = trace.frames();
-    frames
-        .iter()
-        .flat_map(BacktraceFrame::symbols)
-        .skip_while(|s| {
-            s.filename().map(|p| !p.ends_with(curr_file)).unwrap_or(
-                true,
-            ) || s.lineno() != Some(curr_line)
-        })
-        .nth(1 + level as usize)
-        .cloned()
-}
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Copy, Clone)]
-pub enum Code {
+pub(crate) enum Code {
     Ok,
     Error,
     UnrecognizedEnumValue(c_uint),
@@ -57,10 +35,12 @@ impl Code {
         }
     }
 
+    #[allow(dead_code)]
     fn to_c(&self) -> _primitiv::PRIMITIV_C_STATUS {
         unsafe { mem::transmute(self.to_int()) }
     }
 
+    #[allow(dead_code)]
     fn from_c(value: _primitiv::PRIMITIV_C_STATUS) -> Self {
         Self::from_int(value as c_uint)
     }
@@ -83,26 +63,65 @@ impl fmt::Display for Code {
     }
 }
 
-pub struct Status {
+pub(crate) struct Status {
     code: Code,
     message: String,
+    trace: Option<Backtrace>,
 }
 
-//  macro_rules! into_result {
-//      ($expr:expr, $retval:ident) => {
-//          let code: Code::from_int($expr);
-//          match code {
-//              Code::Ok => Ok($retval),
-//              _ => Err(Status::new(code, _primitiv::primitiv_Status_get_message()),
-//          }
-//      }
-//  }
-//
+impl Status {
+    fn new(code: Code, message: String, trace: Option<Backtrace>) -> Self {
+        Status {
+            code: code,
+            message: message,
+            trace: trace,
+        }
+    }
 
-// trait FromApi<T> {
-//     fn from_api_status<T>(status: c_uint, ok_val: T) -> Self;
-// }
-pub trait ApiResult<T, E> {
+    fn code(&self) -> Code {
+        self.code
+    }
+
+    fn message(&self) -> &str {
+        self.message.as_str()
+    }
+}
+
+impl Display for Status {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        try!(write!(
+            f,
+            "Code: \"{}({})\", Message: \"{}\"\n",
+            self.code(),
+            self.code().to_int(),
+            self.message()
+        ));
+        Ok(())
+    }
+}
+
+impl Debug for Status {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        try!(write!(f, "Status: {{"));
+        try!(write!(
+            f,
+            "code: \"{}({})\", message: \"{}\", backtrace: \"\n",
+            self.code(),
+            self.code().to_int(),
+            self.message()
+        ));
+        match self.trace {
+            Some(ref trace) => {
+                try!(write!(f, "{:?}", trace));
+            }
+            None => {}
+        }
+        try!(write!(f, "\n\"}}"));
+        Ok(())
+    }
+}
+
+pub(crate) trait ApiResult<T, E> {
     fn from_api_status(status: c_uint, ok_val: T) -> result::Result<T, E>;
 }
 
@@ -112,195 +131,31 @@ impl<T> ApiResult<T, Status> for result::Result<T, Status> {
         match code {
             Code::Ok => Ok(ok_val),
             _ => unsafe {
+                let trace = Backtrace::new();
+
                 let mut size: usize = 0;
                 let s = _primitiv::primitivGetMessage(ptr::null_mut(), &mut size as *mut _);
                 assert!(Code::is_ok(s));
                 let buffer = CString::new(Vec::with_capacity(size)).unwrap().into_raw();
                 let s = _primitiv::primitivGetMessage(buffer, &mut size as *mut _);
                 assert!(Code::is_ok(s));
-                Err(Status::new(
-                    code,
-                    CString::from_raw(buffer).into_string().unwrap(),
-                ))
+                let message = CString::from_raw(buffer).into_string().unwrap();
+
+                Err(Status::new(code, message, Some(trace)))
             },
         }
     }
 }
 
+pub(crate) type Result<T> = result::Result<T, Status>;
+
 macro_rules! check_api_status {
     ($status:expr) => {
-        Result::from_api_status($status, 0).unwrap();
-    }
-}
-
-pub type Result<T> = result::Result<T, Status>;
-
-impl Status {
-    pub fn new(code: Code, message: String) -> Self {
-        Status {
-            code: code,
-            message: message,
-        }
-        // unsafe {
-        //     let inner = _primitiv::primitiv_Status_new();
-        //     assert!(!inner.is_null());
-        //     Status { inner: inner }
-        // }
-    }
-    //
-    //      /*
-    //      fn from_code<T>(code: Code) -> Result<T, Self> {
-    //  _primitiv::primitiv_Status_get_message()
-    //          Status {}
-    //      }
-    //      */
-    //
-
-    pub fn code(&self) -> Code {
-        self.code
-    }
-
-    pub fn message(&self) -> &str {
-        self.message.as_str()
-    }
-}
-
-/*
-pub struct Status {
-    inner: *mut _primitiv::primitiv_Status,
-}
-
-impl_wrap!(Status, primitiv_Status);
-
-impl Status {
-    pub fn new() -> Self {
-        unsafe {
-            let inner = _primitiv::primitiv_Status_new();
-            assert!(!inner.is_null());
-            Status { inner: inner }
+        match Result::from_api_status($status, 0) {
+            Ok(_) => {},
+            Err(s) => {
+                panic!("{:?}", s);
+            },
         }
     }
 }
-
-impl Drop for Status {
-    fn drop(&mut self) {
-        unsafe {
-            _primitiv::primitiv_Status_delete(self.inner);
-        }
-    }
-}
-
-impl Status {
-    pub fn new_set(code: Code, message: &str) -> Result<Status, NulError> {
-        let symbol = prev_symbol(1);
-        let file = symbol.as_ref().and_then(BacktraceSymbol::filename).unwrap();
-        let line = symbol.as_ref().and_then(BacktraceSymbol::lineno).unwrap();
-        let mut status = Status::new();
-        status.set(code, file.to_str().unwrap(), line, message)?;
-        Ok(status)
-    }
-
-    pub fn code(&self) -> Code {
-        unsafe { Code::from_int(_primitiv::primitiv_Status_get_code(self.as_inner_ptr())) }
-    }
-
-    pub fn is_ok(&self) -> bool {
-        self.code() == Code::Ok
-    }
-
-    pub(crate) fn into_result(self) -> Result<(), Status> {
-        if self.is_ok() { Ok(()) } else { Err(self) }
-    }
-
-    pub fn set(
-        &mut self,
-        code: Code,
-        file: &str,
-        line: u32,
-        message: &str,
-    ) -> Result<(), NulError> {
-        let file = CString::new(file)?;
-        let message = CString::new(message)?;
-        unsafe {
-            _primitiv::primitiv_Status_set_status(
-                self.as_inner_mut_ptr(),
-                code.to_c(),
-                file.as_ptr(),
-                line,
-                message.as_ptr(),
-            );
-        }
-        Ok(())
-    }
-}
-
-macro_rules! invalid_arg {
-    ($fmt:expr) => {
-        Status::new_set(Code::InvalidArgument, $fmt).unwrap()
-    };
-    ($fmt:expr, $($arg:tt)*) => ({
-        let messsag = format!($fmt, $($arg)*);
-        Status::new_set(Code::InvalidArgument, &message).unwrap()
-    });
-}
-
-impl Display for Status {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "{}: ", self.code())?;
-        let message = unsafe {
-            match CStr::from_ptr(_primitiv::primitiv_Status_get_message(self.as_inner_ptr()))
-                .to_str() {
-                Ok(s) => s,
-                Err(_) => "<invalid UTF-8 in message>",
-            }
-        };
-        f.write_str(message)
-    }
-}
-*/
-
-impl Debug for Status {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "{}: ", self.code())?;
-        // let messsage = unsafe {
-        //     match CStr::from_ptr(_primitiv::primitiv_Status_get_message(self.as_inner_ptr()))
-        //         .to_str() {
-        //         Ok(s) => s,
-        //         Err(_) => "<invalid UTF-8 in message>",
-        //     }
-        // };
-        f.write_str(self.message())?;
-        write!(f, "}}")?;
-        Ok(())
-    }
-}
-/*
-
-impl From<NulError> for Status {
-    fn from(_e: NulError) -> Self {
-        invalid_arg!("String contained NUL byte")
-    }
-}
-
-impl From<Utf8Error> for Status {
-    fn from(_e: Utf8Error) -> Self {
-        invalid_arg!("String contained invalid UTF-8")
-    }
-}
-
-impl Error for Status {
-    fn description(&self) -> &str {
-        unsafe {
-            match CStr::from_ptr(_primitiv::primitiv_Status_get_message(self.as_inner_ptr()))
-                .to_str() {
-                Ok(s) => s,
-                Err(_) => "<invalid UTF-8 in message>",
-            }
-        }
-    }
-
-    fn cause(&self) -> Option<&Error> {
-        None
-    }
-}
-*/
