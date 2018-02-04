cd0742604b8814b141875896ddb77a42a858a7fd
Hiroki Teranishi <teranishihiroki@gmail.com>
2018-02-04 14:42:23 +0900

    Implement Initializers.

--- src/initializer.rs
+++ src/initializer.rs
@@ -1,5 +1,25 @@
 use primitiv_sys as _primitiv;
-use Status;
+use ApiResult;
+use Tensor;
 use Wrap;
 
-pub trait Initializer: Wrap<_primitiv::primitiv_Initializer> {}
+/// `Initializer` trait
+pub trait Initializer: Wrap<_primitiv::primitivInitializer_t> {
+    /// Provides an initialized tensor.
+    fn apply(&self, x: &mut Tensor) {
+        unsafe {
+            check_api_status!(_primitiv::primitivApplyInitializer(
+                self.as_ptr(),
+                x.as_mut_ptr(),
+            ));
+        }
+    }
+}
+
+macro_rules! impl_initializer {
+    ($name:ident) => {
+        impl_wrap_owned!($name, primitivInitializer_t);
+        impl_drop!($name, primitivDeleteInitializer);
+        impl Initializer for $name {}
+    }
+}
